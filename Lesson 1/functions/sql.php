8f8306e82b1ee77c0b1211383add5012f3ab042f
Integer64 <tivan@bk.ru>
2015-05-16 21:57:31 +0800

    Add functions for sql

--- Lesson 1/functions/sql.php
+++ Lesson 1/functions/sql.php
@@ -1,1 +1,30 @@
 <?php
+
+function Sql_connect(){
+    $host = '127.0.0.1';
+    $user = 'geekbrainsphp';
+    $password = '123456';
+    $dataBase = 'news_feed';
+
+    mysql_connect($host,$user,$password);
+    mysql_select_db($dataBase);
+    mysql_query('SET NAMES utf8');
+}
+
+function Sql_exec($query){
+    Sql_connect();
+    mysql_query($query);
+}
+
+function Sql_query($query){
+    Sql_connect();
+    $res = mysql_query($query);
+
+    $ret = [];
+
+    while(false !== ($row = mysql_fetch_assoc($res))){
+        $ret[] = $row;
+    }
+    return $ret;
+}
+
