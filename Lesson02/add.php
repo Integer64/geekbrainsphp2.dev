455803392ca1d8eae20b8a3910ed8a7681ed2a65
Integer64 <tivan@bk.ru>
2015-05-25 12:10:18 +0800

    Lesson02. Controller add News.

---
+++ Lesson02/add.php
@@ -0,0 +1,21 @@
+<?php
+require __DIR__ . '/models/News.php';
+
+if(!empty($_POST)){
+    $data = [];
+    if(!empty($_POST['title']) && !empty($_POST['textNews']) && !empty($_POST['date'])){
+        $data['title'] = $_POST['title'];
+        $data['text'] = $_POST['textNews'];
+        $data['date'] = $_POST['date'];
+    }
+
+
+    if(isset($data['title']) && isset($data['text']) && isset($data['date'])){
+     $article = new News();
+     $article->News_addNews($data);
+     header("Location: /Lesson01/");
+      die;
+    }
+}
+
+include __DIR__.'/views/add.php';
