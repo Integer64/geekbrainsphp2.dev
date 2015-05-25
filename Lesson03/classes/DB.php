08b4a0f6870b8d1caa9fd66a6f0205737622c7c0
Integer64 <tivan@bk.ru>
2015-05-26 00:13:20 +0800

    Lesson03. Classes. Complete DB.php which responds for work with database/

--- Lesson03/classes/DB.php
+++ Lesson03/classes/DB.php
@@ -1,5 +1,35 @@
 <?php
 
 class DB {
+    protected $host = '127.0.0.1';
+    protected $user = 'geekbrainsphp';
+    protected $password = '123456';
+    protected $dbName = 'news_feed';
+
+    public function __construct(){
+        mysql_connect($this->host, $this->user, $this->password);
+        mysql_select_db($this->dbName);
+        mysql_query('SET NAMES utf8');
+    }
+
+    public function DB_exec($sql){
+       return mysql_query($sql);
+    }
+
+    public function DB_query($sql){
+        $result = mysql_query($sql);
+        if($result === false){
+            return $result;
+        }
+
+        $ret = [];
+        while(false !== ($row = mysql_fetch_object($result))){
+            $ret[] = $row;
+        }
+
+        return $ret;
+    }
+
+
 
 } 
