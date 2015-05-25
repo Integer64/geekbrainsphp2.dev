0ed5a7e77c2094a523478a21c984ad13c6c99878
Integer64 <tivan@bk.ru>
2015-05-25 12:10:55 +0800

    Lesson02. Controller Index.

--- Lesson02/index.php
+++ Lesson02/index.php
@@ -1,2 +1,6 @@
 <?php
-require __DIR__.'/models/SQL.php';
+require __DIR__.'/models/News.php';
+$news = new News();
+$articles = $news->News_getAllTitleNews();
+
+include __DIR__.'/views/index.php';
