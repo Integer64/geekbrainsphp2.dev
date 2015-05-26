543b149d8c92e312e916fe8249df3b50f88d0f15
Integer64 <tivan@bk.ru>
2015-05-26 20:39:34 +0800

    Add $class

--- Lesson03/models/AbstractArticle.php
+++ Lesson03/models/AbstractArticle.php
@@ -5,26 +5,29 @@ abstract class AbstractArticle {
     public $title;
     public $text;
     public $date;
+    protected static $class;
     private $db;
+
 
     public function __construct(){
         $this->db = new DB();
     }
+
     public function Article_getAll($tableName){
         $sql = "SELECT * FROM ".$tableName;
-        return $this->db->DB_query($sql);
+        $db = $this->db;
+        return $db->DB_query($sql, static::$class);
     }
 
     public function Article_getOne($tableName,$id){
         $sql = "SELECT * FROM ".$tableName." WHERE id = ".$id;
-        return $this->db->DB_query($sql);
+        return $this->db->DB_query($sql, static::$class);
     }
 
     public function Article_add($tableName, $insertColumnsValues){
         foreach($insertColumnsValues as $key => $value){
             $insertColumnsValues[$key] = "'".$value."'";
         }
-
         $sql = 'INSERT INTO '.$tableName.' ('.implode(array_keys($insertColumnsValues),', ').') VALUES ('.implode($insertColumnsValues,',').')';
         $result = $this->db->DB_exec($sql);
         return $result;
