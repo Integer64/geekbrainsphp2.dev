62a7b7edfc9b7ce1ba75c0280c7a1e1d7e01acbd
Integer64 <tivan@bk.ru>
2015-05-27 01:09:15 +0800

    Front controller,

--- Lesson03/index.php
+++ Lesson03/index.php
@@ -1,1 +1,11 @@
 <?php
+require_once __DIR__.'/autoload.php';
+
+$ctrl = isset($_GET['ctrl']) ? $_GET['ctrl'] : 'News';
+$act = isset($_GET['act']) ? $_GET['act'] : 'All';
+
+$cntClassName = $ctrl . 'Controller';
+
+$cnt = new $cntClassName;
+$method = 'action' . $act;
+$cnt->$method();
