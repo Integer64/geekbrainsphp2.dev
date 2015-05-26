ac6b2693d0f9980bfd0c7ea5981f94b42db88155
Integer64 <tivan@bk.ru>
2015-05-27 03:02:03 +0800

    Add view class

--- Lesson03/controllers/NewsController.php
+++ Lesson03/controllers/NewsController.php
@@ -2,13 +2,19 @@
 
 class NewsController {
     public function actionAll(){
-        $articles = News::Article_getAll();
+        $articles = News::getAll();
+        $view = new View();
+        $view->data('news', $articles);
+        $view->display('allNews.php');
         }
 
     public function actionOne(){
-        if(!empty($_POST['id'])){
-            $id = $_POST['id'];
-            $article = News::Article_getOne($id);
+        if(!empty($_GET['id'])){
+            $id = $_GET['id'];
+            $article = News::getOne($id);
+            $view = new View();
+            $view->data('oneNews', $article);
+            $view->display('oneNews.php');
         }else{
             header("Location: /Lesson03/");
         }
