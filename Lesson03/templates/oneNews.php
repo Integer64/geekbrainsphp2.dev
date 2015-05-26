2943422a43679c204b7e47e45f97e5a662aef020
Integer64 <tivan@bk.ru>
2015-05-27 03:01:23 +0800

    Template for one news

---
+++ Lesson03/templates/oneNews.php
@@ -0,0 +1,26 @@
+<?php
+foreach($this->data as $news){
+    $title =$news->title;
+    $text = $news->text;
+    $date = $news->date;
+}
+?>
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+<h1><?php echo $title;?></h1>
+<p>
+    <?php echo $date;?>
+</p>
+<p>
+    <?php echo $text;?>
+</p>
+<p>
+    <a href="/Lesson03/index.php">На главную</a>
+</p>
+</body>
+</html>
