126af604af4c04397247d4223acf8cf13b9ecdaf
Integer64 <tivan@bk.ru>
2015-05-27 03:01:48 +0800

    Class View print views

--- Lesson03/views/View.php
+++ Lesson03/views/View.php
@@ -1,5 +1,16 @@
 <?php
 
-class Views {
+class View {
+    private $type;
+    private $data;
+
+    public function data($type, $items){
+        $this->type = $type;
+        $this->data = $items;
+    }
+
+    public function display($template){
+        include __DIR__.'/../templates/'.$template;
+    }
 
 } 
