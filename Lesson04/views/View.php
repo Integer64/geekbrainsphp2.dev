6a056c356f3d42ef8d359d2ede822304817bf9f1
Integer64 <tivan@bk.ru>
2015-06-03 23:06:13 +0800

    Implemented interface Iterator.

--- Lesson04/views/View.php
+++ Lesson04/views/View.php
@@ -1,10 +1,15 @@
 <?php
 
-class View {
+class View
+    implements Iterator
+{
     protected  $data=[];
+    protected  $keys=[];
+    protected $position = 0;
 
     public function __set($key, $value){
         $this->data[$key] = $value;
+        $this->keys = array_keys($this->data);
     }
 
     public function display($template){
@@ -14,4 +19,28 @@ class View {
         include __DIR__.'/../templates/'.$template;
     }
 
-} 
+    public function current()
+    {
+        return $this->data[$this->keys[$this->position]];
+    }
+
+    public function next()
+    {
+        ++$this->position;
+    }
+
+    public function key()
+    {
+        return $this->keys[$this->position];
+    }
+
+    public function valid()
+    {
+        return isset($this->data[$this->keys[$this->position]]);
+    }
+
+    public function rewind()
+    {
+        $this->position = 0;
+    }
+}
