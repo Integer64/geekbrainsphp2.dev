74e142f25bbe5017b3c8ed3e719928a423fbd2b0
Integer64 <tivan@bk.ru>
2015-06-10 15:54:28 +0800

    Lesson05. autoload

--- Lesson05/autoload.php
+++ Lesson05/autoload.php
@@ -1,6 +1,6 @@
 <?php
 
-function __autoload($class){
+function load($class){
     if(file_exists(__DIR__ . '/controllers/' . $class . '.php')){
         require __DIR__ . '/controllers/' . $class . '.php';
     }else if(file_exists(__DIR__ . '/models/' . $class . '.php')){
@@ -11,3 +11,6 @@ function __autoload($class){
         require __DIR__ . '/views/' . $class . '.php';
     }
 }
+
+spl_autoload_extensions('.php');
+spl_autoload_register('load');
