5ba6d4a338058908afdec507e6e1bbb3c7d17706
Integer64 <tivan@bk.ru>
2015-06-10 15:51:55 +0800

    Lesson05.Use PDO

--- Lesson05/classes/DB.php
+++ Lesson05/classes/DB.php
@@ -1,33 +1,41 @@
 <?php
 
-class DB {
-    protected $host = '127.0.0.1';
-    protected $user = 'geekbrainsphp';
-    protected $password = '123456';
-    protected $dbName = 'news_feed';
+class DB
+{
+    private $dbh;
+    private $className = 'stdClass';
 
-    public function __construct(){
-        mysql_connect($this->host, $this->user, $this->password);
-        mysql_select_db($this->dbName);
-        mysql_query('SET NAMES utf8');
+    public function __construct()
+    {
+        $host = '127.0.0.1';
+        $user = 'geekbrainsphp';
+        $password = '123456';
+        $dbName = 'news_feed';
+        $dsn = 'mysql:host='.$host.';dbname='.$dbName.';charset=utf8';
+
+        $this->dbh = new PDO($dsn,$user,$password);
     }
 
-    public function exec($sql){
-       return mysql_query($sql);
+    public function setClassName($className)
+    {
+        $this->className = $className;
     }
 
-    public function query($sql, $class){
-        $result = mysql_query($sql);
-        if($result === false){
-            return $result;
+    public function exec($sql, $params=[])
+    {
+        $sth = $this->dbh->prepare($sql);
+        $res = $sth->execute($params);
+        if($res){
+            return $this->dbh->lastInsertId();
         }
+        return $res;
+    }
 
-        $ret = [];
-        while(false !== ($row = mysql_fetch_object($result, $class))){
-            $ret[] = $row;
-        }
-
-        return $ret;
+    public function query($sql, $params=[])
+    {
+        $sth = $this->dbh->prepare($sql);
+        $sth->execute($params);
+        return $sth->fetchAll(PDO::FETCH_CLASS, $this->className);
     }
 
 } 
