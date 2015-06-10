7f9a028efe7e7e0d5ad06eb4337b1ce4de90ccf6
Integer64 <tivan@bk.ru>
2015-06-10 15:52:54 +0800

    Lesson05

---
+++ Lesson05/models/NewsModel.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Class NewsModel
+ * @property $id 
+ * @property $title
+ * @property $text
+ * @property $date
+ */
+
+class NewsModel extends AbstractModel
+{
+    protected static $table = 'news';
+} 
