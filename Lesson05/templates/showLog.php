a5d491caa2e4a7abdb2b13849eeb91f2d4f54b35
Integer64 <tivan@bk.ru>
2015-06-12 23:36:03 +0800

    Lesson06 Template Show log file

---
+++ Lesson05/templates/showLog.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/html">
+<head lang="en">
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+    <a href="/Lesson05/">На главную</a>
+<pre>
+    <?php echo $file;?>
+</pre>
+
+</body>
+</html>
